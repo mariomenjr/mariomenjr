cfa78ca30168a3c7a36c1789ea021887a2d4c0f7
Mario Menjívar <mariomenjr@gmail.com>
2020-07-26 00:42:39 -0700

    Make the Hero a link to /
    It makes sense to have a better experience

--- src/components/Header/Hero.svelte
+++ src/components/Header/Hero.svelte
@@ -1,12 +1,14 @@
 <div class="inline-block">
-  <div class="flex flex-row items-center">
-    <img
-      class="rounded-full w-12 sm:w-16"
-      alt="@mariomenjr"
-      src="https://avatars3.githubusercontent.com/u/1946936?s=460&v=4" />
-    <div class="flex flex-col ml-3">
-      <span class="font-bold text-lg sm:text-xl text-grey-800">Mario Menjívar</span>
-      <span class="font-light text-xs text-grey-600 text-left">@mariomenjr</span>
+  <a href="/">
+    <div class="flex flex-row items-center">
+      <img
+        class="rounded-full w-12 sm:w-16"
+        alt="@mariomenjr"
+        src="https://avatars3.githubusercontent.com/u/1946936?s=460&v=4" />
+      <div class="flex flex-col ml-3">
+        <span class="font-bold text-lg sm:text-xl text-grey-800">Mario Menjívar</span>
+        <span class="font-light text-xs text-grey-600 text-left">@mariomenjr</span>
+      </div>
     </div>
-  </div>
+  </a>
 </div>
